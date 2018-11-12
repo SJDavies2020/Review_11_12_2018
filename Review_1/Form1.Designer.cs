70b24282bf947d04eb23fa200f2ee9eed0d03b10
SJDavies2020 <44582668+SJDavies2020@users.noreply.github.com>
2018-11-12 09:28:44 -0500

    Add files via upload
    Review 1 Files

---
+++ Review_1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Review_1
+{
+    partial class frmReview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtVal1 = new System.Windows.Forms.TextBox();
+            this.txtVal2 = new System.Windows.Forms.TextBox();
+            this.lblOne = new System.Windows.Forms.Label();
+            this.lblTwo = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtVal1
+            // 
+            this.txtVal1.Location = new System.Drawing.Point(48, 58);
+            this.txtVal1.Name = "txtVal1";
+            this.txtVal1.Size = new System.Drawing.Size(133, 22);
+            this.txtVal1.TabIndex = 0;
+            // 
+            // txtVal2
+            // 
+            this.txtVal2.Location = new System.Drawing.Point(48, 105);
+            this.txtVal2.Name = "txtVal2";
+            this.txtVal2.Size = new System.Drawing.Size(133, 22);
+            this.txtVal2.TabIndex = 1;
+            // 
+            // lblOne
+            // 
+            this.lblOne.AutoSize = true;
+            this.lblOne.Location = new System.Drawing.Point(45, 38);
+            this.lblOne.Name = "lblOne";
+            this.lblOne.Size = new System.Drawing.Size(93, 17);
+            this.lblOne.TabIndex = 2;
+            this.lblOne.Text = "First Number:";
+            // 
+            // lblTwo
+            // 
+            this.lblTwo.AutoSize = true;
+            this.lblTwo.Location = new System.Drawing.Point(45, 85);
+            this.lblTwo.Name = "lblTwo";
+            this.lblTwo.Size = new System.Drawing.Size(114, 17);
+            this.lblTwo.TabIndex = 3;
+            this.lblTwo.Text = "Second Number:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(51, 142);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(17, 17);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "X";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(48, 172);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 48);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // frmReview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(382, 355);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblTwo);
+            this.Controls.Add(this.lblOne);
+            this.Controls.Add(this.txtVal2);
+            this.Controls.Add(this.txtVal1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmReview";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Review";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVal1;
+        private System.Windows.Forms.TextBox txtVal2;
+        private System.Windows.Forms.Label lblOne;
+        private System.Windows.Forms.Label lblTwo;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ Review_1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Review_1
+{
+    public partial class frmReview : Form
+    {
+    Double MyResult = 0;
+        public frmReview()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            MyResult = (Convert.ToDouble(txtVal1.Text) + Convert.ToDouble(txtVal2.Text)) / 2;
+            lblResult.Text = Convert.ToString(MyResult);
+
+        }
+    }
+}
